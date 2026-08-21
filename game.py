7c7926e595719f21bea17c546e08e14ead074d3a
TheAndromeda <anvimitra@gmail.com>
2026-08-21 21:29:39 +0530

    Add files via upload

--- game.py
+++ game.py
@@ -26,3 +26,42 @@ score_box.move_ip(700,50)
 
 score = 0
 timer_left = 10
+question_file_name = "questions.txt"
+marquee_msg = ""
+is_game_over = False
+answer_boxes = [answer_box1,answer_box2,answer_box3,answer_box4]
+questions = []
+question_count = 0 
+question_index = 0
+
+def draw():
+    global marquee_msg
+    screen.clear()
+    screen.fill("#5D4157")
+    screen.draw.filled_rect(marque_box,"#5D4157")
+    screen.draw.filled_rect(question_box,"#C4CBB7")
+    screen.draw.filled_rect(timer_box,"#D299C2")
+    screen.draw.filled_rect(score_box,"#D299C2")
+    screen.draw.filled_rect(skip_box,"#CFD9DF")
+
+    for answer_box in answer_boxes:
+        screen.draw.filled_rect(answer_box,"#C4CBB7")
+
+    marquee_msg = f"Welcome to Quiz Master, You are at question {question_index}/{question_count}"
+    screen.draw.textbox(marquee_msg,marque_box,color = "white")
+    screen.draw.textbox(f"{timer_left}",timer_box, color ="#43233A", shadow = (0.5,0.5),scolor = "white")
+    screen.draw.textbox(f"score:{score}",score_box, color = "#511740")
+    screen.draw.textbox("SKIP",skip_box,color = "#252A2D", angle = -90)
+    screen.draw.textbox("Hello World",question_box, color = "#4D5146", shadow = (0.5,0.5),scolor = "white")
+    for answer_box in answer_boxes:
+        screen.draw.textbox("hello world",answer_box,color = "#2D302A")
+
+def move_marquee():
+    marque_box.x = marque_box.x - 2
+    if marque_box.right < 0:
+        marque_box.left = WIDTH
+
+def update():
+    move_marquee()
+
+pgzrun.go()
